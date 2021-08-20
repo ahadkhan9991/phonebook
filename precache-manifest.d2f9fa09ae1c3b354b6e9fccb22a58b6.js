f2b0a4c8f000673d596d00d3c83712146dcdb56b
ahadkhan9991 <ahadkhan9991@gmail.com>
2021-08-21 02:18:05 +0530

    Updates

---
+++ precache-manifest.d2f9fa09ae1c3b354b6e9fccb22a58b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caabe9882534d973bd4a8017ce1d3f32",
+    "url": "/phonebook/index.html"
+  },
+  {
+    "revision": "4682892c12c2a05ff55a",
+    "url": "/phonebook/static/css/main.5d6b07b6.chunk.css"
+  },
+  {
+    "revision": "89d15acf615d7791bf2f",
+    "url": "/phonebook/static/js/2.db5faf41.chunk.js"
+  },
+  {
+    "revision": "d0c7295dade8792d4ec795e8fb19eb02",
+    "url": "/phonebook/static/js/2.db5faf41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4682892c12c2a05ff55a",
+    "url": "/phonebook/static/js/main.bcaf5871.chunk.js"
+  },
+  {
+    "revision": "55875b9ff9f941b690dd",
+    "url": "/phonebook/static/js/runtime-main.902715c4.js"
+  }
+]);
